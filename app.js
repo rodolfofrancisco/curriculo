03945143d3a12b58c8069ef5c89014a49aeb2b98
Rodolfo Francisco Pinto <rodolfofrancisco1990@gmail.com>
2014-11-23 20:45:56 -0200

    segundo commit

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    $.ajax({
+        url: 'http://rarolabs.com.br:88/alunos.json',
+        type: 'GET',
+        success: function(data) {
+            $.each(data, function(i, aluno) {
+                $('#curriculos_relacionados tbody').append(
+                    '<tr id="'+aluno.id+'">' +
+                        '<td class="col-md-3 estilo-font-1">' +
+                            '<a href="'+aluno.link_html+'" target="_blank">'+aluno.nome+'</a>' +
+                        '</td>' +
+                        '<td class="col-md-9 estilo-font-1">'+aluno.mini_curriculo+'</td>' +
+                    '<tr>');
+            });
+        },
+        error: function() {
+            console.log('Failed!'); 
+        }
+    });
+    
+});
