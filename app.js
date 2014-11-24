d0039c758f87eb06ca4a76a563042126df6a4d59
rodolfofrancisco <rodolfofrancisco1990@gmail.com>
2014-11-24 10:07:26 -0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $.ajax({
         url: 'http://rarolabs.com.br:88/alunos.json',
         type: 'GET',
-        dataType: 'jsonp',
+        dataType: 'json',
         success: function(data) {
             $.each(data, function(i, aluno) {
                 $('#curriculos_relacionados tbody').append(
